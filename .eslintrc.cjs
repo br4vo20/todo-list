e22966fa6bc32f987768987a749040abab981ac4
Ruben Bravo <rubendosbravo03@gmai.com>
2024-05-02 17:08:30 +0100

    feat: isert eslint in project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,48 @@
+const { version } = require("react");
+const { Extension } = require("typescript");
+
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        paths: 'src',
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      }
+    }
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'simple-import-sort', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'prettier/prettier': ['error', {}, {usePrettierrc: true}],
+    'react/react-in-jsx-scope': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
   },
 }
